5485585a90d41014fb44a03c45c7c50709c89036
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-11-29 13:54:59 +0100

    Add main function

--- main.go
+++ main.go
@@ -9,4 +9,52 @@
 //
 //********************************************************************************************************************//
 
-package uptime_kuma_ping_service
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Response is to return a response
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func main() {
+
+	// Set gin mode
+	gin.SetMode(gin.ReleaseMode)
+
+	// Disable logging
+	gin.DefaultWriter = io.Discard
+
+	// Define router
+	router := gin.New()
+
+	// Add ping pong
+	router.GET("/ping", func(c *gin.Context) {
+
+		// Log to console
+		log.Println("We played a little ping pong.")
+
+		// Return data
+		c.JSON(http.StatusOK, Response{
+			Code:    http.StatusOK,
+			Message: "pong",
+		})
+
+	})
+
+	// Check ssl & start server
+	if os.Getenv("ENABLE_SSL") == "true" {
+		router.RunTLS(":443", "/go/src/app/files/certificates/"+os.Getenv("CERTIFICATE_CRT_NAME"), "/go/src/app/files/certificates/"+os.Getenv("CERTIFICATE_KEY_NAME"))
+	} else {
+		router.Run(":443")
+	}
+
+}
