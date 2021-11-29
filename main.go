42e67d106913f6d7250952239129f21838223200
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-11-29 13:54:04 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+//********************************************************************************************************************//
+//
+// Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
+//
+// This file is part of uptime-kuma-ping-service.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor (aka gowizzard)
+//
+//********************************************************************************************************************//
+
+package uptime_kuma_ping_service
